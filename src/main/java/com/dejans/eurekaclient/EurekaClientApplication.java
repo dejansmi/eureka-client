7c052494799bcd5de8735867dbeef0fe72fc1acc
dejansmi <smiljanic_dejan@hotmail.com>
2018-04-05 01:51:16 +0200

    portManagment

--- src/main/java/com/dejans/eurekaclient/EurekaClientApplication.java
+++ src/main/java/com/dejans/eurekaclient/EurekaClientApplication.java
@@ -1,42 +1,51 @@
 package com.dejans.eurekaclient;
 
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
-import java.util.concurrent.ThreadLocalRandom;
-
-import java.net.URLEncoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.json.JsonReader;
+import javax.json.Json;
+import javax.json.JsonObject;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
+	static final private String clientName = "eureka-client";
+
+	private static JsonObject jsonFromString(String jsonObjectStr) {
+
+		JsonReader jsonReader = Json.createReader(new StringReader(jsonObjectStr));
+		JsonObject object = jsonReader.readObject();
+		jsonReader.close();
+
+		return object;
+	}
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
+		String port = new String();
 		RestTemplate restTemplate = new RestTemplate();
+		long pid = ProcessHandle.current().pid();
 		HttpUtils uriClass = new HttpUtils();
 		String uri = uriClass.setScheme("http").setHost("localhost").setPort("1111").setPath("/portset/")
-				.addParamToQuery("clientName", "eureka-client").addParamToQuery("XZ", "Jugo%$!@#Tugo#$%^&*())\"Sreca").getUrl();
+				.addParamToQuery("clientName", clientName).addParamToQuery("XZ", "Jugo%$!@#Tugo#$%^&*())\"Sreca")
+				.addParamToQuery("pid", pid).getUrl();
 		System.out.println("URI " + uri);
-		String quote = restTemplate.getForObject(uri, String.class);
+		String jsonAnswer = restTemplate.getForObject(uri, String.class);
+		JsonObject jobj = jsonFromString(jsonAnswer);
+		int portInt = jobj.getInt("port");
+		port = Integer.toString(portInt);
 		System.out.println("TEXTIC");
-		System.out.println(quote);
+		System.out.println(jsonAnswer);
 
-		System.out.println(quote);
 		System.out.println("TEXTIC");
-		String port = "4444";
-
-		// nextInt is normally exclusive of the top value,
-		// so add 1 to make it inclusive
-		int randomNum = ThreadLocalRandom.current().nextInt(3000, 10000);
-		port = Integer.toString(randomNum);
 
 		System.setProperty("server.port", port);
 		SpringApplication.run(EurekaClientApplication.class, args);
-		System.out.println(quote);
+		System.out.println(jsonAnswer);
 	}
 }
--- src/main/java/com/dejans/eurekaclient/HttpUtils.java
+++ src/main/java/com/dejans/eurekaclient/HttpUtils.java
@@ -1,8 +1,14 @@
 package com.dejans.eurekaclient;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
 
+import javax.ws.rs.QueryParam;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.util.UriUtils;
 
 public class HttpUtils {
     private String url = new String();
@@ -11,7 +17,11 @@ public class HttpUtils {
     private String host = new String();
     private String port = new String();
     private String path = new String();
+
     private String query = new String();
+    private String currentParamQueryName;
+    private String currentParamQueryValue;
+    private int queryPosition = 0;
 
     // this constructor is used when prepare request
     public HttpUtils() {
@@ -19,8 +29,36 @@ public class HttpUtils {
     }
 
     // this constructor is used when should analize request 
-    public HttpUtils(String url) {
+    public HttpUtils(String url, String queryString) {
         urlForAnalyze = url;
+        if (urlForAnalyze.startsWith("http")) {
+            // this is http protocol
+            scheme = "http";
+            urlForAnalyze = urlForAnalyze.replaceFirst("http://", "");
+            int i = urlForAnalyze.indexOf(":");
+            if (i > -1) {
+                // port exist but first extract host
+                host = urlForAnalyze.substring(0, i);
+                urlForAnalyze = urlForAnalyze.substring(i + 1);
+                // and after that port
+                i = urlForAnalyze.indexOf("/");
+                if (i > -1) {
+                    port = urlForAnalyze.substring(0, i);
+                    urlForAnalyze = urlForAnalyze.substring(i);
+                } else {
+                    // url don't have path so port is rest of url
+                    port = urlForAnalyze;
+                    urlForAnalyze = "";
+                }
+
+            }
+            // rest of urlForAnalyze is path (can be empty string ("")) 
+            path = urlForAnalyze;
+
+            if (queryString != null && queryString != "") {
+                query = queryString;
+            }
+        }
     }
 
     public HttpUtils setScheme(String scheme) {
@@ -28,9 +66,17 @@ public class HttpUtils {
         return this;
     }
 
+    public String getScheme() {
+        return scheme;
+    }
+
     public HttpUtils setHost(String host) {
         this.host = host;
         return this;
+    }
+
+    public String getHost() {
+        return host;
     }
 
     public HttpUtils setPort(String port) {
@@ -38,11 +84,18 @@ public class HttpUtils {
         return this;
     }
 
+    public String getPort() {
+        return port;
+    }
+
     public HttpUtils setPath(String path) {
         this.path = path;
         return this;
     }
 
+    public String getPath() {
+        return path;
+    }
 
     public HttpUtils addParamToQuery(String param, String value) throws UnsupportedEncodingException {
         if (query == null)
@@ -59,13 +112,50 @@ public class HttpUtils {
         return this;
     }
 
+    public HttpUtils addParamToQuery(String param, long value) throws UnsupportedEncodingException {
+        String paramValue = new String();
+        addParamToQuery(param, String.valueOf(value));
+        return this;
+    }
+
+    public String getParamName() throws UnsupportedEncodingException {
+        int i;
+        String param;
+        if (queryPosition >= query.length())
+            return null;
+        i = query.indexOf('&', queryPosition);
+        if (i > -1) {
+            param = query.substring(queryPosition, i);
+        } else {
+            param = query.substring(queryPosition);
+        }
+        int j = param.indexOf('=');
+        if (j > -1) {
+            currentParamQueryName = param.substring(0, j);
+            currentParamQueryValue = URLDecoder.decode(URLDecoder.decode(param.substring(j + 1), "UTF-8"), "UTF-8");
+        } else {
+            currentParamQueryName = param;
+            currentParamQueryValue = "";
+        }
+        queryPosition = i + 1;
+        return currentParamQueryName;
+    }
+
+    public String getParamValue(String nameParam) {
+        if (currentParamQueryName.equals(nameParam)) {
+            return currentParamQueryValue;
+        } else {
+            return "";
+        }
+    }
+
     // this method will be used if you want to clear existing query and start buld a new
-    public HttpUtils newParamsToQuery () {
+    public HttpUtils newParamsToQuery() {
         query = "";
         return this;
     }
 
-    public String getUrl () {
+    public String getUrl() {
         if (scheme == null || !scheme.equals("")) {
             scheme = "http";
         }
